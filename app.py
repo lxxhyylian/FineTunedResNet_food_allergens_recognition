b9399e14f5fb0187ff6936a77057590994c83f19
lxxhyylian <46447860+lxxhyylian@users.noreply.github.com>
2023-11-30 21:05:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,6 @@ if uploaded_file is not None:
                             for j, v in enumerate(output.squeeze())
                             if torch.sigmoid(v) > 0.5])
 
-    st.image(img.squeeze(0), caption="Uploaded Image", use_column_width=True)
+    st.image(img[0], caption="Uploaded Image", use_column_width=True)
     st.write("Class predictions:")
     st.write(pred_title)
